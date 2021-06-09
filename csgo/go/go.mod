be535f11b64b2adafb12d0e9be967bb38c18abf4
Tom <tom.c.maurer@gmail.com>
2021-06-09 14:06:16 -0600

    more parsing framework, basic state data and some event handling

--- csgo/go/go.mod
+++ csgo/go/go.mod
@@ -2,4 +2,7 @@ module github.com/Honestpretzels/csgo_ML_demo_parser
 
 go 1.16
 
-require github.com/markus-wa/demoinfocs-golang/v2 v2.8.1
+require (
+	github.com/markus-wa/demoinfocs-golang v1.11.0 // indirect
+	github.com/markus-wa/demoinfocs-golang/v2 v2.8.1
+)
--- csgo/go/test.go
+++ csgo/go/test.go
@@ -6,7 +6,36 @@ import (
 	"time"
 
 	dem "github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs"
-	events "github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs/events"
+	"github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs/common"
+	"github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs/events"
+)
+
+// enum used to index different aspects of state for a player at a given time
+type playerState int
+
+const (
+	PosX playerState = iota
+	PosY
+	PosZ
+	VelocityX
+	VelocityY
+	VelocityZ
+	ViewX
+	ViewY
+	Health
+	ActiveWeapon
+	Blind
+	Scoped
+	Reloaded
+	Crouched
+	Jumped
+	Shooting
+	Flash
+	Grenade
+	Incendiary
+	Smoke
+	Decoy
+	PlantedBomb
 )
 
 // This contains docs on the parser https://pkg.go.dev/github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs#example-Parser
@@ -14,11 +43,48 @@ import (
 
 func generatePlayerMap(state dem.GameState) map[string]int {
 	var players map[string]int
-	for i := 0; i < len(state.Participants().Playing()); i++ {
-		players[state.Participants().Playing()[i].Name] = i
+	players = make(map[string]int)
+	ct := state.Participants().TeamMembers(state.TeamCounterTerrorists().Team())
+	t := state.Participants().TeamMembers(state.TeamTerrorists().Team())
+	for i := 0; i < len(ct); i++ {
+		players[ct[i].Name] = i
+	}
+	for i := 0; i < len(t); i++ {
+		players[t[i].Name] = 5 + i
 	}
 
 	return players
+}
+
+func boolToFloat(x bool) float64 {
+	if x {
+		return float64(1)
+	}
+	return float64(0)
+}
+
+func updatePlayerState(player *common.Player, second int, idx int, demoVector *[10][2700][23]float64) {
+	// Postion
+	demoVector[idx][second][PosX] = player.Position().X
+	demoVector[idx][second][PosY] = player.Position().Y
+	demoVector[idx][second][PosZ] = player.Position().Z
+	// Velocity
+	demoVector[idx][second][VelocityX] = player.Velocity().X
+	demoVector[idx][second][VelocityY] = player.Velocity().Y
+	demoVector[idx][second][VelocityZ] = player.Velocity().Z
+	// View
+	demoVector[idx][second][ViewX] = float64(player.ViewDirectionX())
+	demoVector[idx][second][ViewY] = float64(player.ViewDirectionY())
+	// Health
+	demoVector[idx][second][Health] = float64(player.Health())
+	// Active Weapon
+	if player.ActiveWeapon() != nil {
+		demoVector[idx][second][ActiveWeapon] = float64(player.ActiveWeapon().Type)
+	}
+	// Blind
+	demoVector[idx][second][Blind] = boolToFloat(player.IsBlinded())
+	// Scoped
+	demoVector[idx][second][Scoped] = boolToFloat(player.IsScoped())
 }
 
 func main() {
@@ -33,27 +99,27 @@ func main() {
 
 	// 10 Players
 	// 2700 = 45 minutes * 60 seconds
-	// timestamp vector = PosX, PosY, PosZ, VelocityX, VelocityY, VelocityZ, ViewX, ViewY, Health, Weapon1, Weapon2, Crouched, Jumped,
-	// Alive, Shooting, Flash, Grenade, Incendiary, Smoke, Decoy, Planted Bomb, Defused Bomb, Blind, Reloaded, Scoped = 25 items
-	// Maybe include buy info for the time when players are buying?
-	// TODO: demoVector := [10][2700][25]float64{}
+	demoVector := [10][2700][23]float64{}
 
 	// Find the start of the match
 	for !p.GameState().IsMatchStarted() {
 		p.ParseNextFrame()
 	}
 
-	// TODO: playerMap := generatePlayerMap(p.GameState())
+	playerMap := generatePlayerMap(p.GameState())
 
 	// TODO: Register handler on each type of event
+
+	var second int
+	second = 0
 	p.RegisterEventHandler(func(e events.WeaponFire) {
-		fmt.Println(e.Weapon.Type)
+		fmt.Println(second)
 	})
 
 	// PARSE TO 45 Minutes
 	startTime := p.CurrentTime()
 out:
-	for second := 0; second < 2700; second++ {
+	for second = 0; second < 2700; second++ {
 		for p.CurrentTime()-startTime < time.Second {
 			EoF, err := p.ParseNextFrame()
 			if err != nil {
@@ -63,7 +129,16 @@ out:
 				break out
 			}
 		}
-		fmt.Println(second)
+		// One second has passed, get all data necessary
+		players := p.GameState().Participants().Playing()
+		for i := 0; i < len(players); i++ {
+			p := players[i]
+			idx := playerMap[p.Name]
+
+			// Store pos and velocity
+			updatePlayerState(p, second, idx, &demoVector)
+
+		}
 
 		startTime = p.CurrentTime()
 	}
