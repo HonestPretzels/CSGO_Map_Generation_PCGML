f206141b7a5d428ca5a3695b0eafbc80a16997b4
Tom <tom.c.maurer@gmail.com>
2021-10-05 16:22:00 -0600

    file splitting and new 15 layer representation

---
+++ csgo/python/fileResizer.py
@@ -0,0 +1,29 @@
+import os, sys
+from os import curdir, path
+import numpy as np
+
+def getAllFiles(p):
+    return [f for f in os.listdir(p) if path.isfile(path.join(p, f))]
+
+def splitAndSave(f, out, n):
+    arr = np.load(f, allow_pickle=True)
+    divisor = len(arr) // n
+    for i in range(n):
+        newArr = arr[i: (i+1)*divisor]
+        np.save("%s_%d.npy"%(out, i+1), newArr,  allow_pickle=True)
+
+def main():
+    inputDir = sys.argv[1]
+    outputDir = sys.argv[2]
+    numSplits = int(sys.argv[3])
+    subFolders = ['breakpoints', 'data', 'maps', 'scores']
+
+    for folder in subFolders:
+        curdir = path.join(inputDir, folder)
+        for f in getAllFiles(curdir):
+            out = path.join(path.join(outputDir, folder), path.basename(f).split(".")[0])
+            splitAndSave(path.join(curdir, f), out, numSplits)
+    pass
+
+if __name__ == "__main__":
+    main()
--- csgo/python/splits_to_images.py
+++ csgo/python/splits_to_images.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 import PIL, sys, os
 from os import path
+from tqdm import tqdm
 
 MAP_SCALE = 128
 
@@ -31,6 +32,7 @@ PLAYER_TYPES = {
     "Standard": 4,
 }
 
+PLAYER_OPACITY = 51 # 255/5 To allow for players to stack on each other
 
 def getAllFilePaths(p):
     return [path.join(p,f) for f in os.listdir(p) if path.isfile(path.join(p, f))]
@@ -48,124 +50,124 @@ def usedUtility(p):
         return True
     return False
 
-def placePlayer(posX, posY, type):
-    points = []
-    if type == PLAYER_TYPES["Utility"]: # H shape
-        # Top Row
-        points.extend([(posX-1, posY+1), (posX+1, posY+1)])
-        # Middle Row
-        points.extend([(posX-1, posY), (posX, posY), (posX+1, posY)])
-        # Bottom Row
-        points.extend([(posX-1, posY-1), (posX+1, posY-1)])
-    elif type == PLAYER_TYPES["Shooting"]: # X Shape
-        # Top Row
-        points.extend([(posX-1, posY+1), (posX+1, posY+1)])
-        # Middle Row
-        points.extend([(posX, posY)])
-        # Bottom Row
-        points.extend([(posX-1, posY-1), (posX+1, posY-1)])
-    elif type == PLAYER_TYPES["Jumping"]: # + Shape
-        # Top Row
-        points.extend([(posX, posY+1)])
-        # Middle Row
-        points.extend([(posX-1, posY), (posX, posY), (posX+1, posY)])
-        # Bottom Row
-        points.extend([(posX, posY-1)])
-    elif type == PLAYER_TYPES["Crouching"]: # O Shape
-        # Top Row
-        points.extend([(posX-1, posY+1), (posX, posY+1), (posX+1, posY+1)])
-        # Middle Row
-        points.extend([(posX-1, posY), (posX+1, posY)])
-        # Bottom Row
-        points.extend([(posX-1, posY-1), (posX, posY-1), (posX+1, posY-1)])
-    elif type == PLAYER_TYPES["Standard"]: # Full Square
-        # Top Row
-        points.extend([(posX-1, posY+1), (posX, posY+1), (posX+1, posY+1)])
-        # Middle Row
-        points.extend([(posX-1, posY), (posX, posY), (posX+1, posY)])
-        # Bottom Row
-        points.extend([(posX-1, posY-1), (posX, posY-1), (posX+1, posY-1)])
+# def placePlayer(posX, posY, type):
+#     points = []
+#     if type == PLAYER_TYPES["Utility"]: # H shape
+#         # Top Row
+#         points.extend([(posX-1, posY+1), (posX+1, posY+1)])
+#         # Middle Row
+#         points.extend([(posX-1, posY), (posX, posY), (posX+1, posY)])
+#         # Bottom Row
+#         points.extend([(posX-1, posY-1), (posX+1, posY-1)])
+#     elif type == PLAYER_TYPES["Shooting"]: # X Shape
+#         # Top Row
+#         points.extend([(posX-1, posY+1), (posX+1, posY+1)])
+#         # Middle Row
+#         points.extend([(posX, posY)])
+#         # Bottom Row
+#         points.extend([(posX-1, posY-1), (posX+1, posY-1)])
+#     elif type == PLAYER_TYPES["Jumping"]: # + Shape
+#         # Top Row
+#         points.extend([(posX, posY+1)])
+#         # Middle Row
+#         points.extend([(posX-1, posY), (posX, posY), (posX+1, posY)])
+#         # Bottom Row
+#         points.extend([(posX, posY-1)])
+#     elif type == PLAYER_TYPES["Crouching"]: # O Shape
+#         # Top Row
+#         points.extend([(posX-1, posY+1), (posX, posY+1), (posX+1, posY+1)])
+#         # Middle Row
+#         points.extend([(posX-1, posY), (posX+1, posY)])
+#         # Bottom Row
+#         points.extend([(posX-1, posY-1), (posX, posY-1), (posX+1, posY-1)])
+#     elif type == PLAYER_TYPES["Standard"]: # Full Square
+#         # Top Row
+#         points.extend([(posX-1, posY+1), (posX, posY+1), (posX+1, posY+1)])
+#         # Middle Row
+#         points.extend([(posX-1, posY), (posX, posY), (posX+1, posY)])
+#         # Bottom Row
+#         points.extend([(posX-1, posY-1), (posX, posY-1), (posX+1, posY-1)])
 
 
         
-    return points
+#     return points
 
 def processArray(seq, maps, images, outPath):
 
-
     # TODO: Iterate through splits
+    # 30 Seconds, 
+    # 15 depth (L1, L2, L1Obj, L2Obj, Bombs, T1_Standard, T1_Crouching, T1_Jumping, T1_Shooting, T1_Utility, T2_Standard, T2_Crouching, T2_Jumping, T2_Shooting, T2_Utility),
+    # Mapsize, 
+    # Mapsize
+    split_outPut = np.zeros((len(seq),30,15, MAP_SCALE, MAP_SCALE), dtype=np.uint8)
+    for split in tqdm(range(len(seq))):
+        split_data = seq[split]
+        split_map = REVERSE_MAP_MAP[maps[split]]
+        L1 = "%s_main_floor.png"%split_map
+        L2 = "%s_floor_2.png"%split_map
+        L1Obj = "%s_main_objects.png"%split_map
+        L2Obj = "%s_objects_2.png"%split_map
+        bombs = "%s_bomb_sites.png"%split_map
+        for imgPath in images:
+            if L1 in imgPath:
+                L1_arr = np.asarray(Image.open(imgPath).convert('L'))
+            elif L2 in imgPath:
+                L2_arr = np.asarray(Image.open(imgPath).convert('L'))
+            elif L1Obj in imgPath:
+                L1Obj_arr = np.asarray(Image.open(imgPath).convert('L'))
+            elif L2Obj in imgPath:
+                L2Obj_arr = np.asarray(Image.open(imgPath).convert('L'))
+            elif bombs in imgPath:
+                bombs_arr = np.asarray(Image.open(imgPath).convert('L'))
 
-    # 30 Seconds, 7 depth (L1, L2, T1, T2, L1Obj, L2Obj, Bombs), Mapsize, Mapsize
-    split_outPut = np.zeros((30,7, MAP_SCALE, MAP_SCALE), dtype=np.uint8)
-    split_data = seq[0]
-    split_map = REVERSE_MAP_MAP[maps[0]]
-    L1 = "%s_main_floor.png"%split_map
-    L2 = "%s_floor_2.png"%split_map
-    L1Obj = "%s_main_objects.png"%split_map
-    L2Obj = "%s_objects_2.png"%split_map
-    bombs = "%s_bomb_sites.png"%split_map
-    for imgPath in images:
-        if L1 in imgPath:
-            L1_arr = np.asarray(Image.open(imgPath).convert('L'))
-        elif L2 in imgPath:
-            L2_arr = np.asarray(Image.open(imgPath).convert('L'))
-        elif L1Obj in imgPath:
-            L1Obj_arr = np.asarray(Image.open(imgPath).convert('L'))
-        elif L2Obj in imgPath:
-            L2Obj_arr = np.asarray(Image.open(imgPath).convert('L'))
-        elif bombs in imgPath:
-            bombs_arr = np.asarray(Image.open(imgPath).convert('L'))
+        for second in range(len(split_data)):
+            split_outPut[split][second][0] = L1_arr
+            split_outPut[split][second][1] = L1Obj_arr
+            split_outPut[split][second][2] = L2_arr
+            split_outPut[split][second][3] = L2Obj_arr
+            split_outPut[split][second][4] = bombs_arr
 
-    for second in range(len(split_data)):
-        split_outPut[second][0] = L1_arr
-        split_outPut[second][1] = L1Obj_arr
-        split_outPut[second][2] = L2_arr
-        split_outPut[second][3] = L2Obj_arr
-        split_outPut[second][4] = bombs_arr
+            for player in range(len(split_data[second])):
+                if player < 5:
+                    teamOffset = 5 # Team 1
+                else:
+                    teamOffset = 10 # Team 2
 
-        for player in range(len(split_data[second])):
-            if player < 5:
-                team = 5 # Team 1
-            else:
-                team = 6 # Team 2
+                player_data = split_data[second][player]
+                if player_data[8] <= 0: # Player is dead
+                    continue
+                else:
+                    posX = round(player_data[0])
+                    posY = round(player_data[1])
+                    print(posY, posX)
+                    # Bump players 1 pixel away from the edge of map to allow for full representations
+                    posX = 126 if posX >= 127 else posX
+                    posY = 126 if posY >= 127 else posY
+                    posX = 1 if posX <= 0 else posX
+                    posY = 1 if posY <= 0 else posY
+                if usedUtility(player_data): # Utility
+                    split_outPut[split][second][teamOffset + 4][posY][posX] += PLAYER_OPACITY
+                elif player_data[15] == 1.0: # Shooting
+                    split_outPut[split][second][teamOffset + 3][posY][posX] += PLAYER_OPACITY
+                elif player_data[14] == 1.0: # Jumped
+                    split_outPut[split][second][teamOffset + 2][posY][posX] += PLAYER_OPACITY
+                elif player_data[13] == 1.0: # Crouched
+                    split_outPut[split][second][teamOffset + 1][posY][posX] += PLAYER_OPACITY
+                else: # Standard
+                    split_outPut[split][second][teamOffset][posY][posX] += PLAYER_OPACITY
+                    
 
-            player_data = split_data[second][player]
-            if player_data[8] <= 0: # Player is dead
-                continue
-            else:
-                posX = round(player_data[0])
-                posY = round(player_data[1])
-                # Bump players 1 pixel away from the edge of map to allow for full representations
-                posX = posX - 1 if posX >= 127 else posX
-                posY = posY - 1 if posY >= 127 else posY
-                posX = posX + 1 if posX <= 0 else posX
-                posY = posY + 1 if posY <= 0 else posY
-            if usedUtility(player_data):
-                pointsToDraw = placePlayer(posX, posY, PLAYER_TYPES["Utility"])
-            elif player_data[15] == 1.0: # Shooting
-                pointsToDraw = placePlayer(posX, posY, PLAYER_TYPES["Shooting"])
-            elif player_data[14] == 1.0: # Jumped
-                pointsToDraw = placePlayer(posX, posY, PLAYER_TYPES["Jumping"])
-            elif player_data[13] == 1.0: # Crouched
-                pointsToDraw = placePlayer(posX, posY, PLAYER_TYPES["Crouching"])
-            else: # Nothing
-                pointsToDraw = placePlayer(posX, posY, PLAYER_TYPES["Standard"])
-            
-            for point in pointsToDraw:
-                split_outPut[second][team][point[1]][point[0]] = 255
-
-        # Visualization Code
-        # r = split_outPut[second][5]
-        # g = split_outPut[second][6]
-        # b = split_outPut[second][0]
-
-
-        # rgbArr = np.dstack((r, g, b))
-        # img = Image.fromarray(rgbArr)
-        # img.show()
+            # Visualization Code
+            # r = split_outPut[split][second][5]
+            # g = split_outPut[split][second][10]
+            # b = split_outPut[split][second][0]
 
 
-    pass
+            # rgbArr = np.dstack((r, g, b))
+            # img = Image.fromarray(rgbArr)
+            # img.show()
+    np.save(outPath, split_outPut)
+
 
 def main():
     splitFolder = sys.argv[1]
@@ -175,14 +177,14 @@ def main():
 
 
     # TODO: Iterate through files
-    firstSplitFile = getAllFilePaths(splitFolder)[0]
-    firstMapFile = getAllFilePaths(mapsFolder)[0]
+    SplitFiles = getAllFilePaths(splitFolder)
+    MapFiles = getAllFilePaths(mapsFolder)
     images = getAllFilePaths(imageFolder)
-    sequenceArr = np.load(firstSplitFile)
-    mapsArr = np.load(firstMapFile)
+    for idx in tqdm(range(len(SplitFiles))):
+        sequenceArr = np.load(SplitFiles[idx])
+        mapsArr = np.load(MapFiles[idx])
 
-    processArray(sequenceArr, mapsArr, images, outFolder)
-    pass
+        processArray(sequenceArr, mapsArr, images, path.join(outFolder, "data_%d.npy"%idx))
 
 if __name__ == "__main__":
     main()
