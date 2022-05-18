fixed some bugs in scaling

csgo/python/Utility/test_loader.py
@@ -21,17 +21,24 @@ def getAllFiles(p):
 #     else:
 #         losses += 1
 # print(wins, losses)
-x = np.load('F:\\CSGO_Map_Generator\\Dev_data_subset\\FullAutoencoderDataSet\\training_data\\data_0_2.npy', allow_pickle=True)
-y = np.load('F:\\CSGO_Map_Generator\\Dev_data_subset\\FullAutoencoderDataSet\\training_labels\\data_0_2.npy', allow_pickle=True)
+x = np.load('F:\\CSGO_Map_Generator\\Dev_data_subset\\FullAutoencoderDataSet\\training_labels_scaled\\data_1_3.npy', allow_pickle=True)
+y = np.load('F:\\CSGO_Map_Generator\\Dev_data_subset\\FullAutoencoderDataSet\\training_data\\data_1_3.npy', allow_pickle=True)
 print(x.shape, y.shape)
 
 # Visualization Code
-for second in range(x.shape[1]):
-    for team in range(x.shape[2]):
-        r = x[0][second][team] * 255
-        g = y[0][second][team] * 255
-        b = np.zeros((128,128))
-        img = np.dstack((r,g,b))
-        r_img = Image.fromarray((img).astype(np.uint8))
-        plt.imshow(r_img)
-        plt.show()
+for split in range(x.shape[0]):
+    for second in range(x.shape[1]):
+        for team in range(x.shape[2]):
+            r = x[split][second][team]
+            print(np.max(r))
+            g = y[split][second][team]
+            b = np.zeros((128,128))
+            
+            s = sum(g.flatten())
+            if s > 1:
+                r *= 255
+                g *= 255
+                img = np.dstack((r,g,b))
+                r_img = Image.fromarray((img).astype(np.uint8))
+                plt.imshow(r_img)
+                plt.show()

csgo/python/test_autoencoder.py
@@ -23,14 +23,15 @@ def visualize(preds, reals):
                 b = preds[y][t][x]
                 print(b.shape)
                 
-                s = sum(b.flatten())
-                if s > 0.1:
-                    b *= 255
-                    r *= 255
-                    img = np.dstack((r,g,b))
-                    r_img = Image.fromarray((img).astype(np.uint8))
-                    plt.imshow(r_img)
-                    plt.show()
+                s = sum(r.flatten())
+                if x in [2,1]:
+                    if s > 0.1:
+                        r *= 255
+                        b *= 255
+                        img = np.dstack((r,g,b))
+                        r_img = Image.fromarray((img).astype(np.uint8))
+                        plt.imshow(r_img)
+                        plt.show()
             
     
 def test(datasetPath, labelsPath, checkpointPath):
@@ -42,7 +43,7 @@ def test(datasetPath, labelsPath, checkpointPath):
     testDataSet = tf.data.Dataset.from_generator(
         generator=lambda: generate_batches(testDataFiles, testDataLabels, batchSize),
         output_types=(np.float32, np.float32),
-        output_shapes=([batchSize,3,128,128], [batchSize,2,128,128])
+        output_shapes=([batchSize,3,128,128], [batchSize,3,128,128])
     )
     
     AE,_ = genModel()

csgo/python/train_autoencoder.py
@@ -14,6 +14,8 @@ WEIGHT_CONSTANT = np.ones((BATCH_SIZE, 15, 128, 128), dtype=np.float32)
 # Multiply the Non-main levels by 100 to increase their importance relative to the main floor
 WEIGHT_CONSTANT[:,0:5,:,:] *= 0
 WEIGHT_CONSTANT = tf.convert_to_tensor(WEIGHT_CONSTANT, dtype=tf.float32)
+SEPARATE_TEAMS = True
+
 
 def weighted_MSE(y_true, y_pred):
     # Copy to keep the originals as they are supposed to be
@@ -38,7 +40,11 @@ def generate_batches(dataFiles, labelFiles, batch_size):
         data = np.load(xname, allow_pickle=True)
         data = data.reshape(data.shape[0]*30, 3, 128, 128)
         labels = np.load(yname, allow_pickle=True)
-        labels = labels.reshape(labels.shape[0]*30, 3, 128, 128)
+        if SEPARATE_TEAMS:
+            labels = labels.reshape(labels.shape[0]*30, 3, 128, 128)
+        else:
+            labels = labels.reshape(labels.shape[0]*30, 2, 128, 128)
+            
         maxLength = data.shape[0] - (data.shape[0] % batch_size)
         for i in range(0, maxLength, batch_size):
             x = data[i:i+batch_size]
@@ -66,10 +72,11 @@ def genModel():
     decoded = layers.Conv2DTranspose(32, (3,3), strides=2, activation="relu", data_format="channels_first", padding="same")(decoded)
     decoded = layers.Conv2DTranspose(16, (3,3), strides=2, activation="relu", data_format="channels_first", padding="same")(decoded)
     decoded = layers.Conv2DTranspose(8, (3,3), strides=2, activation="relu", data_format="channels_first", padding="same")(decoded)
-    decoded = layers.Conv2D(3, (3,3), activation="relu", padding="same", data_format="channels_first")(decoded)
-    # encoded = layers.Flatten()(encoded)
-    # decoded = layers.Dense(32, activation="relu")(encoded)
-    # decoded = layers.Dense(1, activation="linear")(encoded)
+    if SEPARATE_TEAMS:
+        decoded = layers.Conv2D(3, (3,3), activation="relu", padding="same", data_format="channels_first")(decoded)
+    else:
+        decoded = layers.Conv2D(2, (3,3), activation="relu", padding="same", data_format="channels_first")(decoded)
+        
     autoencoder = keras.Model(input_img, decoded)
     encoder = keras.Model(input_img, latent)
 
@@ -100,19 +107,25 @@ def main():
 
     trainDataX = [path.join(trainDataDir, f) for f in getAllFiles(trainDataDir)]
     trainDataY = [path.join(trainLabelsDir, f) for f in getAllFiles(trainLabelsDir)]
-
     
     batchSize = BATCH_SIZE # Number of single seconds to look at
 
     
-
-    trainDataSet = tf.data.Dataset.from_generator(
-        generator=lambda: generate_batches(trainDataX, trainDataY, batchSize),
-        output_types=(np.float32, np.float32),
-        output_shapes=([batchSize,3,128,128], [batchSize,3,128,128])
-    )
+    if SEPARATE_TEAMS:
+        trainDataSet = tf.data.Dataset.from_generator(
+            generator=lambda: generate_batches(trainDataX, trainDataY, batchSize),
+            output_types=(np.float32, np.float32),
+            output_shapes=([batchSize,3,128,128], [batchSize,3,128,128])
+        )
+    else:
+            trainDataSet = tf.data.Dataset.from_generator(
+            generator=lambda: generate_batches(trainDataX, trainDataY, batchSize),
+            output_types=(np.float32, np.float32),
+            output_shapes=([batchSize,3,128,128], [batchSize,2,128,128])
+        )
 
     autoencoder, _ = genModel()
+    autoencoder.summary()
     train(autoencoder, trainDataSet, 30, checkpointPath)
 
 if __name__ == "__main__":